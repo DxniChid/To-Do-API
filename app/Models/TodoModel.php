e9d5e8c5f086e5b126734591c8ed5ef0245d1d34
Joseph <167524188+Joseph051006@users.noreply.github.com>
2026-04-29 14:16:44 +0200

    Update TodoModel.php

--- app/Models/TodoModel.php
+++ app/Models/TodoModel.php
@@ -43,50 +43,36 @@ class TodoModel extends Model
     protected $afterFind = [];
     protected $beforeDelete = [];
     protected $afterDelete = [];
-
     /**
      * Get Filtered Data
      * 
      * @param array $filter
      * @return array
      */
-
-
-public function getFiltered($filter){
-    $return = array();
-    $builder = $this->db->table($this->table);
-    if(!empty($filter['limit'])){
-        if(!empty($filter['offset'])){
-            $builder->limit($filter['limit'], $filter['offset']);
+    public function getFiltered($filter)
+    {
+        $return = array();
+        $builder = $this->db->table($this->table);
+        if (!empty($filter['limit'])) {
+            if (!empty($filter['offset'])) {
+                $builder->limit($filter['limit'], $filter['offset']);
+            } else {
+                $builder->limit($filter['limit']);
+            }
         }
-        else{
-            $builder->limit($filter['limit']);
+        if (!empty($filter['order'])) {
+            $builder->orderBy($filter['order']);
         }
+        if (!empty($filter['version'])) {
+            $builder->where('version', $filter['version']);
+        }
+        $query = $builder->get();
+        $return['total'] = $builder->countAll();
+        $return['data'] = array();
+        foreach ($query->getResultArray() as $row) {
+            $return['data'][$row[$this->primaryKey]] = $row;
+        };
+        return $return;
     }
-
-    if(!empty($filter['order'])){
-        $builder->orderBy($filter['order']);
-    }
-
-    if(!empty($filter['version'])){
-        $builder->where('version', $filter['version']);
-    }
-
-    $query = $builder->get();
-
-    $return['total'] = $builder->countAll();
-
-    $return['data'] = array();
-    
-    foreach ($query->getResultArray() as $row){
-        $return['data'][$row[$this->primaryKey]] = $row;
-    };
-
-    return $return;
-
-
-
-
-}
 
 }
