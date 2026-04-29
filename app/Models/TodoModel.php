59af05c2087f27f066a24ccace0912d9f14596e1
Joseph <167524188+Joseph051006@users.noreply.github.com>
2026-04-29 14:13:16 +0200

    created Filter
    Created Some variables with a function inside the TodoModel.php controller

--- app/Models/TodoModel.php
+++ app/Models/TodoModel.php
@@ -6,13 +6,13 @@ use CodeIgniter\Model;
 
 class TodoModel extends Model
 {
-    protected $table            = 'todos';
-    protected $primaryKey       = 'id';
+    protected $table = 'todos';
+    protected $primaryKey = 'id';
     protected $useAutoIncrement = true;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $returnType = 'array';
+    protected $useSoftDeletes = false;
+    protected $protectFields = true;
+    protected $allowedFields = ['version'];
 
     protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
@@ -22,25 +22,71 @@ class TodoModel extends Model
 
     // Dates
     protected $useTimestamps = false;
-    protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
+    protected $dateFormat = 'datetime';
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = 'deleted_at';
 
     // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
+    protected $validationRules = [];
+    protected $validationMessages = [];
+    protected $skipValidation = false;
     protected $cleanValidationRules = true;
 
     // Callbacks
     protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
+    protected $beforeInsert = [];
+    protected $afterInsert = [];
+    protected $beforeUpdate = [];
+    protected $afterUpdate = [];
+    protected $beforeFind = [];
+    protected $afterFind = [];
+    protected $beforeDelete = [];
+    protected $afterDelete = [];
+
+    /**
+     * Get Filtered Data
+     * 
+     * @param array $filter
+     * @return array
+     */
+
+
+public function getFiltered($filter){
+    $return = array();
+    $builder = $this->db->table($this->table);
+    if(!empty($filter['limit'])){
+        if(!empty($filter['offset'])){
+            $builder->limit($filter['limit'], $filter['offset']);
+        }
+        else{
+            $builder->limit($filter['limit']);
+        }
+    }
+
+    if(!empty($filter['order'])){
+        $builder->orderBy($filter['order']);
+    }
+
+    if(!empty($filter['version'])){
+        $builder->where('version', $filter['version']);
+    }
+
+    $query = $builder->get();
+
+    $return['total'] = $builder->countAll();
+
+    $return['data'] = array();
+    
+    foreach ($query->getResultArray() as $row){
+        $return['data'][$row[$this->primaryKey]] = $row;
+    };
+
+    return $return;
+
+
+
+
+}
+
 }
