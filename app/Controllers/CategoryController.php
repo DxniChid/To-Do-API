4d081d096a67fc7d7a1f90bc22548649107e87c6
DxniChid <chidiroglou.danai06@gmail.com>
2026-05-27 14:51:26 +0200

    Fixes

--- app/Controllers/CategoryController.php
+++ app/Controllers/CategoryController.php
@@ -10,23 +10,50 @@ class CategoryController extends ResourceController
 {
     protected $modelName = CategoryModel::class;
     protected $format = 'json';
+    public function index()
+{
+    return $this->respond($this->model->findAll());
+}
 
+
+public function create()
+{
+    $data = $this->request->getJSON(true);
+
+    if (!isset($data['name']) || strlen(trim($data['name'])) < 2) {
+        return $this->failValidationErrors([
+            'name' => 'Name required (min 2 chars)'
+        ]);
+    }
+
+    $data['name'] = trim(strip_tags($data['name']));
+
+    $this->model->insert($data);
+
+    return $this->respondCreated([
+        'message' => 'Kategorie erstellt '
+    ]);
+}
     public function delete($id=null)
     {
         if (!is_numeric($id)) {
             return $this->failValidationErrors([
-            'id' => 'Invalid ID'
+            'id' => 'ungültige ID'
         ]);
         }
         if (! $this->model->find($id)) {
-            return $this->failNotFound('Category not found');
+            return $this->failNotFound('Kategorie nicht gefunden');
         }
         $todo = new TodoModel();
 
         if ($todo->where('category_id',$id)->countAllResults()>0) {
-            return $this->fail('Category not empty',409);
+            return $this->fail('Kategorie nicht leer',409);
         }
 
-        return $this->respondDeleted($this->model->delete($id));
+        $this->model->delete($id);
+
+        return $this->respondDeleted([
+             'message' => 'Kategorie erfolgreich gelöscht'
+        ]);
     }
 }
